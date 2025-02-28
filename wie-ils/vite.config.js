4d393c63da0d0302b32fde31a69f5c372be1c5e8
Sahab Noor <sahabnoor193@gmail.com>
2025-02-28 17:05:55 +0500

    Hosting

--- wie-ils/vite.config.js
+++ wie-ils/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/<WIE>/', // For example, base: '/my-project/'
+  base: '/WIE/', // For example, base: '/my-project/'
   plugins: [react()],
 })
