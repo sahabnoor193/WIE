9b97272ff60c3dcfd0a70aac989bd779c855d906
Sahab Noor <sahabnoor193@gmail.com>
2025-02-28 17:22:43 +0500

    Hosting

--- wie-ils/src/App.jsx
+++ wie-ils/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
 
-     <Router>
+     <Router basename="/WIE">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
